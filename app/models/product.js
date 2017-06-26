6d44ff8aa35b7c51bf62eaf1032aaa3f4ed87475
slammyde7113 <dyers1@wit.edu>
2017-06-26 15:11:12 -0400

    Added Products and Carts Table Added Products tables that holds item information. Added Carts table to hold references to Users and Products tables. Created instance of a test product to see if table was properly created. Carts table was not checked yet

---
+++ app/models/product.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  _owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  toJSON: {
+    virtuals: true,
+    transform: function (doc, ret, options) {
+      const userId = (options.user && options.user._id) || false
+      ret.editable = userId && userId.equals(doc._owner)
+      return ret
+    }
+  }
+})
+
+const Product = mongoose.model('Product', productSchema)
+
+module.exports = Product
---
+++ scripts/products/create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/products"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}"
+  --data '{
+    "product": {
+      "name": "'"${TEXT}"'",
+      "price": "'"${NUM}"'",
+      "description": "'"${TEXT}"'"
+    }
+  }'
+
+echo
