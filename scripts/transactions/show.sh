089484608425e2164bd7f53dd609061e96ed594a
slammyde7113 <dyers1@wit.edu>
2017-07-12 19:42:24 -0400

    Transactions Show Curl Script Successful: Tested
    tri-programmed: @RoryEnright, @isharma-sharma, @slammyde7113
    
    Made a curl request for showing individual transactions. Requires
    a transaction id in the url and an authentication token.

--- scripts/transactions/show.sh
+++ scripts/transactions/show.sh
@@ -3,9 +3,10 @@
 API="http://localhost:4741"
 URL_PATH="/transactions"
 
-curl "${API}${URL_PATH}/${ID}" \
+curl "${API}${URL_PATH}/5966b2aa2392122ff4d87a69" \
   --include \
   --request GET \
-  --header "Authorization: Token token=$TOKEN"
+  --header "Authorization: Token token=8+O/qn2kKIU22SEj2x6yE+AfxZFIbGSnlXVx9HLBim4=--yCQRSYXDrrpAIpJNtuITk/qW30HVYUyEu7AgaGylQCo=" \
 
 echo
+#EMAIL=a PASSWORD=a
